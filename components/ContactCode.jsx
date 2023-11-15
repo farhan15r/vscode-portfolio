8afb5ae7316477c592c4be9b03464b22264404f9
farhan ramadhan <nak.rokesselalu@gmail.com>
2023-11-15 21:04:28 +0700

    update contact

--- components/ContactCode.jsx
+++ components/ContactCode.jsx
@@ -3,53 +3,23 @@ import styles from '../styles/ContactCode.module.css';
 const contactItems = [
   {
     social: 'website',
-    link: 'nitinranganath.me',
-    href: 'https://nitinranganath.me',
+    link: 'farhan15r.me',
+    href: 'https://www.farhan15r.me',
   },
   {
     social: 'email',
-    link: 'nitinranganath@gmail.com',
-    href: 'mailto:nitinranganath@gmail.com',
+    link: 'farhan15ramadhan@gmail.com',
+    href: 'mailto:farhan15ramadhan@gmail.com',
   },
   {
     social: 'github',
-    link: 'itsnitinr',
-    href: 'https://github.com/itsnitinr',
+    link: 'farhan15r',
+    href: 'https://github.com/farhan15r',
   },
   {
     social: 'linkedin',
-    link: 'nitinranganath',
-    href: 'https://www.linkedin.com/in/nitinranganath/',
-  },
-  {
-    social: 'twitter',
-    link: 'iamnitinr',
-    href: 'https://www.twitter.com/iamnitinr',
-  },
-  {
-    social: 'instagram',
-    link: 'iamnitinr',
-    href: 'https://www.instagram.com/iamnitinr',
-  },
-  {
-    social: 'polywork',
-    link: 'nitinranganath',
-    href: 'https://www.polywork.com/nitinranganath',
-  },
-  {
-    social: 'telegram',
-    link: 'iamnitinr',
-    href: 'https://t.me/iamnitinr',
-  },
-  {
-    social: 'codepen',
-    link: 'nitinranganath',
-    href: 'https://codepen.io/itsnitinr',
-  },
-  {
-    social: 'codesandbox',
-    link: 'itsnitinr',
-    href: 'https://codesandbox.io/u/itsnitinr',
+    link: 'Farhan Ramadhan',
+    href: 'https://www.linkedin.com/in/farhan15r/',
   },
 ];
 
--- pages/contact.jsx
+++ pages/contact.jsx
@@ -3,28 +3,28 @@ import ContactCode from '../components/ContactCode';
 import styles from '../styles/ContactPage.module.css';
 
 const ContactPage = () => {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [subject, setSubject] = useState('');
-  const [message, setMessage] = useState('');
+  // const [name, setName] = useState('');
+  // const [email, setEmail] = useState('');
+  // const [subject, setSubject] = useState('');
+  // const [message, setMessage] = useState('');
 
-  const submitForm = async (e) => {
-    e.preventDefault();
-    console.log(process.env.NEXT_PUBLIC_API_URL);
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/contact`, {
-      method: 'POST',
-      body: JSON.stringify({ name, email, subject, message }),
-    });
-    if (res.ok) {
-      alert('Your response has been received!');
-      setName('');
-      setEmail('');
-      setSubject('');
-      setMessage('');
-    } else {
-      alert('There was an error. Please try again in a while.');
-    }
-  };
+  // const submitForm = async (e) => {
+  //   e.preventDefault();
+  //   console.log(process.env.NEXT_PUBLIC_API_URL);
+  //   const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/contact`, {
+  //     method: 'POST',
+  //     body: JSON.stringify({ name, email, subject, message }),
+  //   });
+  //   if (res.ok) {
+  //     alert('Your response has been received!');
+  //     setName('');
+  //     setEmail('');
+  //     setSubject('');
+  //     setMessage('');
+  //   } else {
+  //     alert('There was an error. Please try again in a while.');
+  //   }
+  // };
 
   return (
     <div className={styles.container}>
@@ -32,7 +32,7 @@ const ContactPage = () => {
         <h3 className={styles.heading}>Reach Out Via Socials</h3>
         <ContactCode />
       </div>
-      <div>
+      {/* <div>
         <h3 className={styles.heading}>Or Fill Out This Form</h3>
         <form className={styles.form} onSubmit={submitForm}>
           <div className={styles.flex}>
@@ -83,7 +83,7 @@ const ContactPage = () => {
           </div>
           <button type="submit">Submit</button>
         </form>
-      </div>
+      </div> */}
     </div>
   );
 };
